76e32f335f053859a173b905fc2063500e9e0cbd
LucasLemos <lucaslfarah25@gmail.com>
2025-06-04 17:32:56 -0300

    base config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
     env: {
         API_URL: `${process.env.NEXT_PUBLIC_API_URL}`,
     },
-    images: {
-        domains: ['clieent-bucket-s3.s3.sa-east-1.amazonaws.com'],
-    },
+
     crossOrigin: 'anonymous',
 }
 
