eaa23544b8c6022aaaa7f371a1b1aa75daa40d7b
LucasLemos <lucaslfarah25@gmail.com>
2025-06-10 23:00:55 -0300

    fix outoplay

--- pages/index.tsx
+++ pages/index.tsx
@@ -116,7 +116,7 @@ export default function Home() {
                         width="100%"
                         height="352"
                         frameBorder="0"
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                        allow=" clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                         loading="lazy"
                     ></iframe>
                 </S.Playlist>
