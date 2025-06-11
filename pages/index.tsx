5259350112cd8b697571cb973bc8c9549a84dc00
LucasLemos <lucaslfarah25@gmail.com>
2025-06-11 12:28:44 -0300

    fix iframe

--- pages/index.tsx
+++ pages/index.tsx
@@ -114,9 +114,9 @@ export default function Home() {
                         className="iframe"
                         src="https://open.spotify.com/embed/playlist/4I5rPKPogPvjW94EGzbaul?utm_source=generator&theme=0"
                         width="100%"
-                        height="152"
+                        height="352"
                         frameBorder="0"
-                        allow=" clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                        allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                         loading="lazy"
                     ></iframe>
                 </S.Playlist>
